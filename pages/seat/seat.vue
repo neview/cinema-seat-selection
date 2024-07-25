32504dab1a07f3b7423c7d0ca8dd15d6fa5b18dd
芒果集团 <1282261308@qq.com>
2024-07-25 17:39:34 +0800

    '拖拽'

--- pages/seat/seat.vue
+++ pages/seat/seat.vue
@@ -7,9 +7,10 @@
     <view class="hall_name render_nimation_anim">
       vip 影厅
     </view>
-    <movable-area  class="seating_map" :style="{transform: `translateX(${xValue}px) translateY(${yValue}px)`}">
-      <movable-view class="movable_view" :scale="true" :scale-min="0.8" :scale-max="1.5" :scale-value="scaleValue" @change="dragChange" @scale="scaleChange">
-          <view v-for="(item,index) in seatLists" :key="index" class="row_list render_nimation_anim" @touchstart="touchstart" @touchmove="touchmove" @touchend="touchend">
+    <movable-area  class="seating_map">
+      <movable-view class="movable_view" direction="all" :scale="true" :scale-min="1" :scale-max="1.3" :scale-value="scaleValue" @change="dragChange" @scale="scaleChange">
+        <view :style="{transform: `translateX(${xValue}px) translateY(${yValue}px)`}" @touchstart="touchstart" @touchmove="touchmove" @touchend="touchend">
+          <view v-for="(item,index) in seatLists" :key="index" class="row_list render_nimation_anim">
             <view v-for="(item2,index2) in item" :key="index2" class="columnNo_list" @click="selectSeat(item2)">
               <!-- 普通座位 -->
               <block v-if="item2.lovestatus == 0">
@@ -91,9 +92,10 @@
               </block>
             </view>
           </view>
+        </view>
       </movable-view>
     </movable-area>
-    <view class="grade_list grade_show_anim" :style="{top:gradeTop+'px',transform:'scale('+gradeScale+')'}">
+    <view class="grade_list grade_show_anim" :style="{top:gradeTop+'px',transform:`scale(${gradeScale}) translateY(${yValue}px)`}">
       <span v-for="item in gradeList" :key="item" :style="{height:itemImgSize+'px'}">{{item}}</span>
     </view>
 	</view>
@@ -231,19 +233,18 @@ import seatDate from './seat.json'
         this.gradeTop = obj.detail.top
       },
       touchstart(event){
-        const firstTouch = event.touches[0];
-        this.startX = firstTouch.clientX;
-        this.startY = firstTouch.clientY;
+          const firstTouch = event.touches[0];
+          this.startX = firstTouch.clientX;
+          this.startY = firstTouch.clientY;
       },
       touchmove(event){
         const firstTouch = event.changedTouches[0];
-        const deltaX = firstTouch.clientX - this.startX;
-        const deltaY = firstTouch.clientY - this.startY;
-        console.log('deltaX',deltaX)
-        console.log('deltaY',deltaY)
+        this.xValue = firstTouch.clientX - this.startX;
+        this.yValue = firstTouch.clientY - this.startY;
       },
-      touchend(obj){
-        console.log('离开',obj)
+      touchend(){
+        this.xValue = 0
+        this.yValue = 0
       },
       /*
        *  选择座位
@@ -304,7 +305,7 @@ import seatDate from './seat.json'
 }
 .seating_map{
   width: 100%;
-  height: 600rpx;
+  height: 50vh;
   overflow: hidden;
   .movable_view{
     width: 100%;
